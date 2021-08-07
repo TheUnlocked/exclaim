16a30b0b4db199cbe975d597b9a3465c4581d14c
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2021-08-07 15:41:21 -0700

    Fixed $context.follow

--- src/codegen/CodeGenerator.ts
+++ src/codegen/CodeGenerator.ts
@@ -310,8 +310,7 @@ export class CodeGenerator extends BaseASTVisitor<string> implements ASTVisitor<
     }
 
     private followAction(code: string): string {
-        // return `$context.follow=$context.follow.finally(($context=>async()=>${code})({...$context}))`;
-        return `$context.follow=${code}`;
+        return `$context.follow=$context.follow.then($r=($context=>async()=>${code})({...$context}),$r)`;
     }
 
     visitSend(ast: Send): string {
